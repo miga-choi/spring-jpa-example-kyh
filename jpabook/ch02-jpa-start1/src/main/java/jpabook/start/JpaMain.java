8efd28c9f95bb54bcf1230e4d84ef4ab8ab991b1
miga-choi <miga.choi.1004@gmail.com>
2024-05-01 23:15:57 +0900

    try "Transaction"

--- jpabook/ch02-jpa-start1/src/main/java/jpabook/start/JpaMain.java
+++ jpabook/ch02-jpa-start1/src/main/java/jpabook/start/JpaMain.java
@@ -14,16 +14,17 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
         // [엔티티 매니저] - 생성
         EntityManager em = emf.createEntityManager();
+
         // [트랜잭션] - 획득
-        EntityTransaction tx = em.getTransaction();
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션 API
 
         try {
-            tx.begin();  // [트랜잭션] - 시작
-            logic(em);   // 비즈니스 로직
-            tx.commit(); // [트랜잭션] - 커밋
+            tx.begin();  // 트랜잭션 시작
+            logic(em);   // 비즈니스 로직 실행
+            tx.commit(); // 트랜잭션 커밋
         } catch (Exception e) {
             e.printStackTrace();
-            tx.rollback(); // [트랜잭션] - 롤백
+            tx.rollback(); // 예외 발생 시 트랜잭션 롤백
         } finally {
             em.close(); // [엔티티 매니저] - 종료
         }
