1979e6780119ed88161ac855142f2a38806c0467
miga-choi <miga.choi.1004@gmail.com>
2024-05-19 23:51:35 +0900

    modify "testSave" func

--- jpabook/ch04-jpa-start2/src/main/java/jpabook/start/JpaMain.java
+++ jpabook/ch04-jpa-start2/src/main/java/jpabook/start/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.start;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,7 +18,8 @@ public class JpaMain {
 
         try {
             tx.begin(); // 트랜잭션 시작
-            logic(em);  // 비즈니스 로직
+            // logic(em);  // 비즈니스 로직
+            testSave(em);
             tx.commit();// 트랜잭션 커밋
         } catch (Exception e) {
             e.printStackTrace();
@@ -54,7 +56,7 @@ public class JpaMain {
         em.remove(member);
     }
 
-    public void testSave(EntityManager em) {
+    public static void testSave(EntityManager em) {
         // 팀1 저장
         Team team1 = new Team("team1", "팀1");
         em.persist(team1);
@@ -62,11 +64,21 @@ public class JpaMain {
         // 회원1 저장
         Member member1 = new Member("member1", "회원1");
         member1.setTemm(team1); // 연관관계 설정 member1 -> team1
+        member1.setAge(30);
+        member1.setDescription("");
+        member1.setCreatedDate(new Date());
+        member1.setLastModifiedDate(new Date());
+        member1.setRoleType(RoleType.USER);
         em.persist(member1);
 
         // 회원2 저장
         Member member2 = new Member("member2", "회원2");
         member2.setTemm(team1); // 연관관계 설정 member2 -> team1
+        member2.setAge(30);
+        member2.setDescription("");
+        member2.setCreatedDate(new Date());
+        member2.setLastModifiedDate(new Date());
+        member2.setRoleType(RoleType.USER);
         em.persist(member2);
     }
 
