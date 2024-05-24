b018e88eca3a636015426ccedf5dd2ae75ef946a
miga-choi <miga.choi.1004@gmail.com>
2024-05-24 22:54:39 +0900

    try saving both two related entities

--- jpabook/ch05-jpa-start2/src/main/java/jpabook/start/JpaMain.java
+++ jpabook/ch05-jpa-start2/src/main/java/jpabook/start/JpaMain.java
@@ -1,7 +1,6 @@
 package jpabook.start;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,7 +22,8 @@ public class JpaMain {
             // testDelete(em);
             // biDirection(em);
             // testSave2(em);
-            testSaveNonOwner(em);
+            // testSaveNonOwner(em);
+            test_ORM_양방향(em);
             tx.commit(); // 트랜잭션 커밋
         } catch (Exception e) {
             e.printStackTrace();
@@ -152,6 +152,87 @@ public class JpaMain {
         team1.getMembers().add(member2);
 
         em.persist(team1);
+    }
+
+    /**
+     * 그렇다면 정말 연관관계의 주인에만 값을 저장하고 주인이 아닌 곳에는 값을 저장하지 않아도 될까?
+     * 사실은 객체 관점에서 양쪽 방향에 모두 값을 입력해주는 것이 가장 안전하다.
+     * 양쪽 방향 모두 값을 입력하지 않으면 JPA를 사용하지 않는 순수한 객체 상태에서 심각한 문제가 발생할 수 있다.
+     * ------------------------------------------------------------------------
+     * 예를 들어 JPA를 사용하고 않고 엔티티에 대한 테스트 코드를 작성한다고 가정해보자.
+     * ORM은 객체와 관계형 데이터베이스 둘 다 중요하다.
+     * 데이터베이스뿐만 아니라 객체도 함께 고려해야 한다.
+     */
+    public static void test_순수한객체_양방향1() {
+        // 팀1
+        Team team1 = new Team("team1", "팀1");
+
+        // 회원1
+        Member member1 = new Member("member1", "회원1");
+
+        // 회원2
+        Member member2 = new Member("member2", "회원2");
+
+        member1.setTemm(team1); // 연관관계 설정 member1 -> team1
+        member2.setTemm(team1); // 연관관계 설정 member2 -> team1
+
+        List<Member> members = team1.getMembers();
+        System.out.println("members.size = " + members.size());
+        // 결과: members.size = 0
+    }
+
+    /**
+     * 양방향은 양쪽다 관계를 설정해야 한다.
+     * 회원 -> 팀을 성정 하면 반대방향인 팀 -> 회원도 설정해야 한다.
+     */
+    public static void test_순수한객체_양방향2() {
+        // 팀1
+        Team team1 = new Team("team1", "팀1");
+
+        // 회원1
+        Member member1 = new Member("member1", "회원1");
+
+        // 회원2
+        Member member2 = new Member("member2", "회원2");
+
+        member1.setTemm(team1);          // 연관관계 설정 member1 -> team1
+        team1.getMembers().add(member1); // 연관관계 설정 team1 -> member1
+
+        member2.setTemm(team1);          // 연관관계 설정 member2 -> team1
+        team1.getMembers().add(member2); // 연관관계 설정 team1 -> member2
+
+        List<Member> members = team1.getMembers();
+        System.out.println("members.size = " + members.size());
+        // 결과: members.size = 2
+    }
+
+    /**
+     * 양쪽에 연관관계를 설정했다. 따라서 순수한 객체 상태에서도 동작하며, 테이블의 외래 키도 정상 입력된다.
+     * 물론 외래 키의 값은 연관관계의 주인인 Member.team 값을 사용한다.
+     * - Member.team: 연관관계의 주인, 이 값으로 외래 키를 관리한다.
+     * - Team.members: 연관관계의 주인이 아니다. 따라서 저장 시에 사용되지 않는다.
+     * 결론: 객체의 양방향 연관관계는 양쪽 모두 관계를 맺어주자.
+     */
+    public static void test_ORM_양방향(EntityManager em) {
+        // 팀1 저장
+        Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
+
+        // 회원1
+        Member member1 = new Member("member1", "회원1");
+
+        // 양방향 연관관계 설장
+        member1.setTemm(team1);          // 연관관계 설정 member1 -> team1
+        team1.getMembers().add(member1); // 연관관계 설정 team1 -> member1
+        em.persist(member1);
+
+        // 회원2
+        Member member2 = new Member("member2", "회원2");
+
+        // 양방향 연관관계 설장
+        member2.setTemm(team1);          // 연관관계 설정 member2 -> team1
+        team1.getMembers().add(member2); // 연관관계 설정 team1 -> member2
+        em.persist(member2);
     }
 
 }
