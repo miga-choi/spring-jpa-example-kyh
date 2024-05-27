44b3bef6f2e95afc77458debd4f8b4a571f59468
miga-choi <miga.choi.1004@gmail.com>
2024-05-28 00:40:06 +0900

    modify bidirectional ManyToOne relationship

---
+++ jpabook/ch06-jpa-start1-2/src/main/java/jpabook/start/Member.java
@@ -0,0 +1,47 @@
+package jpabook.start;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    // Getter, Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+
+        // 무한루프에 빠지지 않도록 체크
+        if (!team.getMembers().contains(this)) {
+            team.getMembers().add(this);
+        }
+    }
+}
