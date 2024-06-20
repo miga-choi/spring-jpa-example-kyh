a55aeef61801bde4be2307339ad6146e00368886
miga-choi <miga.choi.1004@gmail.com>
2024-06-20 22:56:06 +0900

    try "eager loading"

---
+++ jpabook/ch08-jpa-start2-1/src/main/java/jpabook/start/Member.java
@@ -0,0 +1,41 @@
+package jpabook.start;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @Column(name = "MEMBER_ID")
+    private String id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    // Getter, Setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
